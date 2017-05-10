31af47fffba452c72786cf29f45100f0c607c60a
eleroy <leroy.eric@gmail.com>
2017-05-10 17:15:19 -0400

    Added Details

--- app/src/main/java/com/wewow/projectwithluis/detail/DetailPresenter.java
+++ app/src/main/java/com/wewow/projectwithluis/detail/DetailPresenter.java
@@ -4,11 +4,16 @@ package com.wewow.projectwithluis.detail;
  * Created by eleroy on 5/10/2017.
  */
 
-public class DetailPresenter {
+public class DetailPresenter implements DetailContract.Presenter{
 
     private DetailContract.View mView;
 
     DetailPresenter(DetailContract.View view) {
         mView = view;
+    }
+
+    @Override
+    public void start() {
+
     }
 }
